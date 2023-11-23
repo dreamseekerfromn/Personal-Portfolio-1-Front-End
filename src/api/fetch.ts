3fa7e830fb2b8ec2dc0f763d48fd570fa98700af
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2023-11-22 21:12:02 -0500

    fix errata

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 const serverURL = import.meta.env.VITE_BASE_URL;
 
 export async function postAuth(body:userinfoInterface){
-    return await axios.post(`${serverURL}/users/login`, body);
+    return await axios.post(`${serverURL}/user/login`, body);
 }
 
 export async function postNewUser(body:userinfoInterface){
-    return await axios.post(`${serverURL}/users`, body);
+    return await axios.post(`${serverURL}/user`, body);
 }
